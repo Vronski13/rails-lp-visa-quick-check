9f5916979406bee2bbe2d4b7d31ba1d1cbcbf28f
Verena Mueller <verena.mller@gmail.com>
2020-05-13 19:44:44 +0200

    Created User Controller with new and create + matching routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "users/new", to: "users#new"
+  post "users", to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
