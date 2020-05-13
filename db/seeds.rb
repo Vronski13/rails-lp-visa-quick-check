0f94c4b9d97c5a0b127353468885d5f91651f1ff
Verena Mueller <verena.mller@gmail.com>
2020-05-13 18:24:51 +0200

    Added faker gem and created first seed - creating 5 fake Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating 5 Users"
+
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    origin: ["C1", "C2", "C3"].sample,
+    qualification: ["vocational", "university", "none"].sample,
+    german_graduation: [true, false].sample,
+    job_offer: [true, false].sample
+  )
+end
+
+puts "Finished!"
